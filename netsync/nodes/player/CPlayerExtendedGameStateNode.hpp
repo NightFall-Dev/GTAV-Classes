742f0d7a71acfbe2365ab57dbaa8c3d7c99ae6ca
NightFall-Dev <23613531+NightFall-Dev@users.noreply.github.com>
2026-07-28 20:02:06 +0800

    Add cleaned files
    (cherry picked from commit 98c2c1959e2ba129ccb6268f24f89b49596bbd2b)

--- netsync/nodes/player/CPlayerExtendedGameStateNode.hpp
+++ netsync/nodes/player/CPlayerExtendedGameStateNode.hpp
@@ -8,8 +8,12 @@ class CPlayerExtendedGameStateNode : CSyncDataNodeInfrequent
 public:
     float waypoint_x;         //0x00C0
     float waypoint_y;         //0x00C4
-    uint16_t waypoint_entity; //0x00C5
+    bool waypoint_entity;     //0x00C5
     bool has_active_waypoint; //0x00C6
     bool owns_waypoint;       //0x00C7
+    bool unk4;                //0x00C8
+    bool unk5;                //0x00C9
+    bool has_waypoint_data;   //0x00CA
+    bool is_waypoint_set;     //0x00CB
 };
 #pragma pack(pop)
